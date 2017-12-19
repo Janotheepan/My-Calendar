eda0c35b710e0a4929e8ee2ea3a01ec035139b07
Mariyathas Janotheepan <mariyathas_j@3logixs.lk>
2017-12-19 12:34:40 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+// import { AppState } from './app.service';
+import * as moment from 'moment' ;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  public date = moment();
+  public  daysArr;
+  constructor() {}
+
+  public ngOnInit() {
+    this.daysArr = this.createCalendar(this.date);
+  }
+  public todayCheck(day) {
+    if(!day){
+      return false;
+    }
+    return moment().format('L') === day.format('L');
+  }
+  public createCalendar(month) {
+    let firstDay = moment(month).startOf('M');
+    let days = Array.apply(null, {length: month.daysInMonth()})
+      .map(Number.call, Number)
+      .map((n) => {
+        return moment(firstDay).add(n, 'd');
+      });
+    for (let n = 0; n < firstDay.weekday(); n++) {
+      days.unshift(null);
+    }
+    return days;
+  }
+    public nextMonth(){
+      this.date.add(1, 'M');
+      this.daysArr = this.createCalendar(this.date);
+    }
+    public previousMonth(){
+      this.date.subtract(1, 'M');
+      this.daysArr = this.createCalendar(this.date);
+    }
 }
